4db9d0652db3bc27fa97c61f62238098a9cce6f4
Eldar Kalachev <rusgunter@gmail.com>
2020-02-05 01:49:22 +0300

    Spring Boot JPA (Hibernate): adding a DB to the web application

---
+++ src/main/java/com/example/sweater/MainController.java
@@ -0,0 +1,48 @@
+package com.example.sweater;
+
+import com.example.sweater.domain.Message;
+import com.example.sweater.repositories.MessagesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class MainController {
+    @Autowired
+    private MessagesRepository messagesRepository;
+
+    @GetMapping
+    public String messages(Map<String, Object> model) {
+        Iterable<Message> messages = messagesRepository.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping
+    public String messages(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messagesRepository.save(message);
+
+        Iterable<Message> messages = messagesRepository.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model){
+        Iterable<Message> messages = filter.isEmpty()
+                ? messagesRepository.findAll()
+                : messagesRepository.findByTag(filter);
+        
+        model.put("messages", messages);
+
+        return "main";
+    }
+}
---
+++ src/main/java/com/example/sweater/repositories/MessagesRepository.java
@@ -0,0 +1,10 @@
+package com.example.sweater.repositories;
+
+import com.example.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessagesRepository extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
